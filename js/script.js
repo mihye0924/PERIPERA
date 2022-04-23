5ca9957a401ca44b83615329df5d2445c4bc860c
mihye0924 <mihye0924@naver.com>
2022-04-23 21:20:11 +0900

    d

--- js/script.js
+++ js/script.js
@@ -331,4 +331,27 @@ $(document).ready(function(){
           }
         }) 
          
-      })
+
+        var tag = document.createElement('script');
+
+        tag.src = "https://www.youtube.com/embed/uiRcKpBGhhw?";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        var player;
+        function onYouTubeIframeAPIReady() {
+          player = new YT.Player('player', {
+            width: '100%',
+            videoId: 'YOUR_VIDEO_ID',
+            playerVars: { 'autoplay': 1, 'playsinline': 1 },
+            events: {
+              'onReady': onPlayerReady
+            }
+          });
+        }
+ 
+        function onPlayerReady(event) {
+          event.target.mute();
+          event.target.playVideo();
+        }
+    })
