afbb6e2d2b89d63acbff2c484d4981e330dac636
mihye0924 <mihye0924@naver.com>
2022-04-24 14:47:12 +0900

    d

--- js/script.js
+++ js/script.js
@@ -346,14 +346,14 @@ $(document).ready(function(){
     var player;
     var player2;     
     function onYouTubeIframeAPIReady(){
-      player = new YT.Player('player',{ 
-        videoId:'uiRcKpBGhhw?playlist=uiRcKpBGhhw&autoplay=1&mute=1&loop=1',
+      player = new YT.Player('player',{   
+        videoId:'uiRcKpBGhhw?playlist=uiRcKpBGhhw&autoplay=1&mute=1&loop=1&origin=https://mihye0924.github.io/PERIPERA',
         playerVars : { 'autoplay' : 1,  'controls': 0, 'loop': 1, 'origin':'https://mihye0924.github.io/PERIPERA' },
         events : { 'onReady' : onPlayerReady }
       });
 
       player2 = new YT.Player('player2',{ 
-        videoId:'0Sp4gLXorgY?playlist=0Sp4gLXorgY&autoplay=1&mute=1&loop=1', 
+        videoId:'0Sp4gLXorgY?playlist=0Sp4gLXorgY&autoplay=1&mute=1&loop=1&origin=https://mihye0924.github.io/PERIPERA', 
         playerVars : { 'autoplay' : 1, 'controls': 0, 'loop': 1, 'origin':'https://mihye0924.github.io/PERIPERA' },
         events : { 'onReady' : onPlayerReady2 }
       });
