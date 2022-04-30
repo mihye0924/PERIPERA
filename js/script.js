6a577430b5dd7a0012f88d6ab493a1cee8c2a1e0
mihye0924 <mihye0924@naver.com>
2022-04-30 16:16:36 +0900

    d

--- js/script.js
+++ js/script.js
@@ -76,7 +76,7 @@ $(document).ready(function(){
       $(this).css('display','none')
       $('.pasueBtn').css('display','inline-block')
       
-      $('#video')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*')
+      // $('#video')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*')
     
     })
     
